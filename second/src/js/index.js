cdef3f57f86c4f4dc8716c09af038cfbfb15886c
Urroke <kovalevyura1@gmail.com>
2019-11-30 12:07:28 +0600

    make desktop

--- second/src/js/index.js
+++ second/src/js/index.js
@@ -1,6 +1,7 @@
 let n = 1;
-
 let sliding = false;
+let map, header, filterBlock;
+let stickAll;
 
 const endSlide = (oldSlide, newSlide) => {
     sliding = false;
@@ -22,9 +23,44 @@ const slide = (direct) => {
         children[prevn].setAttribute('class', 'description__img description__img_active_move_l');
         children[n].setAttribute('class', 'description__img description__img_active_semi');
     }
-    console.log(n);
 }
 
 const openCloseFilter = (oc) => {
     document.getElementById("filter").style.display = oc ? ("block"):("none");
 }
+
+const stickComponent = (sticked, stickedInitState, stickTo) => {           /// Хотелось сделать, так чтобы работало с адаптивными элементами без css
+    if(sticked.display === "none") return;
+    const stickConst = stickTo.clientHeight + stickTo.offsetTop;
+    if (sticked.offsetTop - stickConst  <= scrollY){ 
+    sticked.style.position = "fixed";
+    if(stickTo.style.position === "relative")
+        sticked.style.top = stickConst - scrollY + "px";   
+    else  sticked.style.top = stickConst + "px";
+    sticked.style.right = 0;};
+    if(stickedInitState - stickConst > scrollY){ sticked.style.position = "relative" ;sticked.style.top = 0;};
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    map = document.getElementById("map-container");
+    header = document.getElementById("main-header");
+    filterBlock = document.getElementById("filter-block");
+    filterInitialOffset = filterBlock.offsetTop;
+    mapInitialOffset = map.offsetTop;
+
+    stickAll = () => {
+        stickComponent(filterBlock, filterInitialOffset, header);
+        stickComponent(map, mapInitialOffset, filterBlock);
+    }
+
+    document.addEventListener("scroll", () => {      
+        stickAll();
+    });
+
+}, false);
+
+
+window.addEventListener("resize", () => {
+    stickAll()
+  }, false);
+
