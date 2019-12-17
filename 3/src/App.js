72104105f3fe9ea32513baafebf239e7f6db0954
Urroke <kovalevyura1@gmail.com>
2019-12-17 13:56:44 +0600

    make 3

---
+++ 3/src/App.js
@@ -0,0 +1,15 @@
+import React,
+    {
+    Component
+}
+
+from "react";
+import "./App.sass";
+
+class App extends Component {
+    render() {
+        return (<div> <h1>A2 Design React Internship</h1> </div>);
+    }
+}
+
+export default App;
